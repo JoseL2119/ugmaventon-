4c4f22e55dc402da11e154c842226e2dc90dbcc4
Jamal25-ia <76519029+Jamal25-ia@users.noreply.github.com>
2024-12-26 00:34:22 -0400

    CREATE TRAVEL YA SUBE A BD +VER BD y Crear Driver
    LOS ODIO MUCHO VAGOS DE MRD PERO ESTO NADIE LO LERA

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,20 +3,37 @@ import 'package:ugmaventon/pages/login_page.dart';
 import '../pages/register_page.dart';
 import '../pages/type_register.dart'; // Importamos la pantalla de selección
 import '../pages/register_passenger.dart'; // Importamos la pantalla de pasajeros
+import '../pages/create_travel.dart'; // Importamos la pantalla de crear aventones
+import '../pages/mapatest.dart'; // testing
+import '../pages/Elegir_Aventon.dart'; // testing
+
+import '../pages/Mapa.dart'; // Ajusta la ruta según la ubicación real del archivo
 
 class AppRoutes {
-  static const initialRoute = '/login';
+  static const initialRoute = '/create_travel';
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/login':
         return MaterialPageRoute(builder: (context) => const LoginPage());
+      case '/Select_Travel':
+        return MaterialPageRoute(builder: (context) => Select_Travel());
+      case '/create_travel':
+        return MaterialPageRoute(builder: (context) => CreateTravel());
       case '/register':
         return MaterialPageRoute(builder: (context) => const RegisterPage());
       case '/type_register':
-        return MaterialPageRoute(builder: (context) => const TypeRegisterPage());
+        return MaterialPageRoute(
+            builder: (context) => const TypeRegisterPage());
       case '/register_passenger':
-        return MaterialPageRoute(builder: (context) => const RegisterPassengerPage());
+        return MaterialPageRoute(
+            builder: (context) => const RegisterPassengerPage());
+      case '/mainMapa':
+        return MaterialPageRoute(builder: (context) => const MyApp_Mapa());
+
+      case '/mapatest':
+        return MaterialPageRoute(builder: (context) => const MainMapaTest());
+
       default:
         return MaterialPageRoute(
           builder: (context) => const Scaffold(
